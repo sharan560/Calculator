4dea7b69b4d120a0c3b88fd54675cff78100b835
Sharan Nagarajan <sharan.nagarajan@multicorewareinc.com>
2026-08-06 11:51:09 +0530

    Basic Structure of the project

--- main.cpp
+++ main.cpp
@@ -2,5 +2,34 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello World";
+    cout<<"Calculator  Application";
+
+    int input1,input2;
+    cout<<"Enter the input 1\n";
+    cin>>input1;
+    cout<<"Enter the input 2\n";
+    cin>>input2;
+    int choice;
+    cout<<"1.Addition \n 2.Subraction \n 3. Multiplication 4.Division\n";
+    cout<<"Enter the Option\n";
+    cin>>choice;
+
+    switch(choice){
+        case 1:
+          
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            break;
+        case 4:
+           
+            break; 
+        default:
+            cout<<"Wrong choice";       
+    }
+
+
+
 }
